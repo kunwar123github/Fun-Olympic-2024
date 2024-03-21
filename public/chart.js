8c79d348441cb936218d58ffb89dbd163266db2c
kunwar123github <nclblkb123@gmail.com>
2024-03-21 08:34:55 +0545

    first commit

--- public/chart.js
+++ public/chart.js
@@ -1,12 +1,20 @@
 const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
-    type: 'bar',
+    type: 'pie', // Change the chart type to 'pie'
     data: {
-        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'Sept', 'Oct', 'Nov', 'Dec',],
+        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June'],
         datasets: [{
             label: 'No. of Users',
             data: [12, 19, 13, 25, 12, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(255, 206, 86, 0.7)',
+                'rgba(75, 192, 192, 0.7)',
+                'rgba(153, 102, 255, 0.7)',
+                'rgba(255, 159, 64, 0.7)'
+            ],
             borderWidth: 1,
         }]
     },
